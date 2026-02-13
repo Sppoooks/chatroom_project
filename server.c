cf2b83df8a6f97bbff679cffd58a85826a0c6b1d
king von <thomas.wann14@gmail.com>
2026-02-12 18:41:37 -1000

    Added multiple client functionality

--- server.c
+++ server.c
@@ -3,10 +3,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <errno.h>
+#include <poll.h>
+#include <unistd.h>
 
-int main() {
+#define port 9999
+#define MAX_CLIENTS 100
 
-  int port_number = 9999; // server listening on port 2000
+int main(void) {
+
   char *server_ip = "";   // empty string for local host
 
   int server_socket_fd =
@@ -15,7 +20,7 @@ int main() {
   struct sockaddr_in address;
 
   address.sin_family = AF_INET;
-  address.sin_port = htons(port_number);
+  address.sin_port = htons(port);
   inet_pton(
       AF_INET, server_ip,
       &address.sin_addr.s_addr); // convert ipv4 address to network convention
@@ -29,20 +34,63 @@ int main() {
   listen(server_socket_fd,
          10); // marks server_socket as a passive listening socket
 
-  int client_socket_fd = accept(server_socket_fd, 0, 0);
+  struct pollfd fds[MAX_CLIENTS + 1];
+  fds[0].fd = server_socket_fd;
+  fds[0].events = POLLIN;
 
-  char
-      buffer[1024]; // hold up to 1024 ASCII characters, less than 1024 in utf-8
+  int nfds = 1;
 
-  while (true) {
+  while(1) {
 
-    ssize_t amount_recieved = recv(client_socket_fd, buffer, 1024, 0);
+    int ready = poll(fds, nfds, -1);
 
-    if (amount_recieved > 0) {
-      buffer[amount_recieved] = 0;
-      printf("%s", buffer);
+    if (ready < 0) {
+      perror("poll");
+      break;
+    }
+
+    for (int i = 0; i < nfds; i++) {
+      if (fds[i].revents & POLLIN) {
+        if (fds[i].fd == server_socket_fd) {
+          int client_fd = accept(server_socket_fd, NULL, NULL);
+
+          fds[nfds].fd = client_fd;
+          fds[nfds].events = POLLIN;
+          nfds++;
+
+          printf("New client connected\n");
+        }
+      else {
+          char buffer[1024];
+          int bytes = read(fds[i].fd, buffer, sizeof(buffer));
+
+          if (bytes <= 0) {
+
+            printf("Client %d disconnected\n", fds[i].fd);
+            close(fds[i].fd);
+          }
+          else {
+            buffer[bytes] = '/0';
+
+            printf("%s", buffer);
+          }
+        }
+      }
     }
   }
+  // char
+  //     buffer[1024]; // hold up to 1024 ASCII characters, less than 1024 in utf-8
+
+  // while (true) {
+  //
+  //   ssize_t amount_recieved = recv(client_socket_fd, buffer, 1024, 0);
+  //
+  //   if (amount_recieved > 0) {
+  //     buffer[amount_recieved] = 0;
+  //     printf("%s", buffer);
+  //   }
+  // }
+  close(server_socket_fd);
 
   return 0;
 }
--- test.c
+++ test.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <poll.h>
 
 int main() {
-  char *line = NULL;
-  size_t size = 0;
-  ssize_t char_count = getline(&line, &size, stdin);
-  printf("char_count %zd:\n", char_count);
-  free(line);
+  struct pollfd fds[1];
+  
+  // Monitor standard input
+  fds[0].fd = STDIN_FILENO;
+  fds[0].events = POLLIN;
+
+  printf("Waiting for input (5 seconds)... \n");
+  
+  int ret = poll(fds, 1, 5000);
+
+  if (ret == -1) {
+    perror("poll\n");
+    return 1;
+  }
+
+  else if (ret == 0) {
+    printf("Timeout! no input recieved.\n");
+  }
+
+  else {
+    if (fds[0].revents & POLLIN) {
+      printf("Data is available to read!\n");
+
+      char buffer[100];
+      ssize_t bytes = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+      if (bytes > 0) {
+        buffer[bytes] = '\0';
+        printf("You typed: %s\n", buffer);
+      }
+    }
+  }
+  return 0;
 }
